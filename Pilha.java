9cd55e9f0113b913829da3648e92a3bd461254ad
Jefferson Mateus <jeffersonmateus64@gmail.com>
2020-12-23 16:27:31 -0300

    Inicio aulas

---
+++ Pilha.java
@@ -0,0 +1,28 @@
+
+/**
+ *
+ * @author jefferson
+ */
+public class Pilha {
+
+    private int valores[];
+    private int topo;
+
+    public Pilha() {
+
+    }
+
+    public void push(int elemento) {
+
+    }
+
+    public boolean isEmpty() {
+        return ;
+    }
+
+    public boolean isFull() {
+    }
+
+    public int pop() {
+    }
+}
